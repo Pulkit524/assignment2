e085b43d14c9270d370c650a0760a6bc9e32dafa
pulkit524 <Patelpulkit524@gmail.com>
2020-10-25 08:55:40 +0530

    commit site || all

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword = 'mongodb+srv://pulkit1234:pulkit1234@cluster0.70zvn.mongodb.net/users?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
+
+// dbPassword = 'mongodb+srv://utkarsh:utk98@cluster0.ao51t.mongodb.net/users?retryWrites=true&w=majority';
